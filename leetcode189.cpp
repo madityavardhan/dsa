4f204fda1c11f361619333278b3afa2c5deac7a4
madityavardhan <108918086+madityavardhan@users.noreply.github.com>
2024-07-16 10:54:46 +0530

    refined leetcode 189

--- leetcode189.cpp
+++ leetcode189.cpp
@@ -2,7 +2,7 @@ class Solution {
 public:
     void rotate(vector<int>& nums, int k) {
         int n=nums.size();
-        if(n<=1) return;
+        k%=n;
         // while(k--){
         //     // swap last element to 1st (Gives TLE)
         //     for(int i=n-1;i>0;i--){
@@ -12,7 +12,7 @@ public:
 
         // 1,2,3,4,5,6,7 -> 7,6,5,4,3,2,1 -> 5,6,7,1,2,3,4
         reverse(nums.begin(),nums.end());
-        reverse(nums.begin(),nums.begin()+(k%n));
-        reverse(nums.begin()+(k%n),nums.end());
+        reverse(nums.begin(),nums.begin()+k);
+        reverse(nums.begin()+k,nums.end());
     }
 };
